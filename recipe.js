ba155eb86b227c755ab249a489274a853f58ff1d
l-0123 <156991064+l-0123@users.noreply.github.com>
2024-03-28 20:01:30 +0530

    first draft of project

---
+++ recipe.js
@@ -0,0 +1,12 @@
+function submitFeedback() {
+    const feedback = document.getElementById('feedbackInput').value;
+    // Here you can send the feedback to your server or perform any other action
+    alert('Thank you for your feedback: ' + feedback);
+}
+/* Existing JavaScript functions remain the same */
+
+function submitNewsletter() {
+    const email = document.getElementById('emailInput').value;
+    // Here you can handle the newsletter subscription
+    alert('Thank you for subscribing to our newsletter!');
+}
